c76dc04b16087b9b0d66c428b526b1b682417717
Victor Santos <victords895@gmail.com>
2022-08-04 13:07:16 -0300

    vuex add and remove country from state and layout changes

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,6 +3,11 @@ import { useQuery } from "@vue/apollo-composable";
 import { computed, ref } from "vue";
 import { useStore } from "vuex";
 import gql from "graphql-tag";
+import ListStatusIcon from "vue-material-design-icons/ListStatus.vue";
+import EarthIcon from "vue-material-design-icons/Earth.vue";
+import PlusCircleIcon from "vue-material-design-icons/PlusCircle.vue";
+import CloseThickIcon from "vue-material-design-icons/CloseThick.vue";
+
 export default {
   setup() {
     const { result } = useQuery(gql`
@@ -37,58 +42,98 @@ export default {
       store.dispatch("actionAddCountryToList", payload);
     }
 
+    function removeCountryFromList(payload) {
+      store.dispatch("actionRemoveCountryFromList", payload);
+    }
+
     return {
       search,
       filteredCountries,
       addCountryToList,
+      removeCountryFromList,
       store,
     };
+  },
+  // icons components
+  components: {
+    ListStatusIcon,
+    EarthIcon,
+    PlusCircleIcon,
+    CloseThickIcon,
   },
 };
 </script>
 
 <template>
-  <header class="sm:container mx-auto flex justify-between m-12">
-    <h1>Travel List</h1>
-    <input
-      class="text-black w-1/4"
-      type="text"
-      v-model="search"
-      placeholder="Search"
-    />
-    <h3>Result(s): {{ filteredCountries.length }}</h3>
-    <a class="hover:text-amber-400" href="#"
-      >My List({{ store.state.listCount }})</a
-    >
-  </header>
-  <aside>
-    <h1>teste</h1>
-  </aside>
-
-  <main class="flex justify-center flex-wrap gap-10 mt-2">
-    <div
-      class="border-collapse rounded-full group relative"
-      v-for="country in filteredCountries"
-      :key="country.code"
-    >
-      <img
-        class="h-64 w-auto"
-        v-bind:src="`https://source.unsplash.com/1600x900/?${country.name}-nature`"
-      />
-      <div
-        class="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-gray-700 opacity-0 group-hover:h-full group-hover:opacity-90 duration-500"
+  <div class="flex">
+    <div class="flex-1 ml-5">
+      <header
+        class="font-bold sm:container mx-auto flex items-center justify-between my-12"
       >
-        <h1 class="text-2xl text-white">
-          {{ country.name }}
-        </h1>
-        <button
-          class="mt-5 px-8 py-3 rounded-full bg-amber-400 hover:bg-amber-600 duration-300"
-          href="#"
-          v-on:click="addCountryToList(country)"
+        <div class="flex gap-2">
+          <EarthIcon />
+          <h1>Travel List</h1>
+        </div>
+        <input
+          class="text-black w-1/4 rounded-md border border-gray-300 px-4 py-1"
+          type="text"
+          v-model="search"
+          placeholder="Search Country..."
+        />
+        <h3>Country Result(s): {{ filteredCountries.length }}</h3>
+      </header>
+      <main class="flex justify-center flex-wrap gap-10 mt-2">
+        <div
+          class="group relative"
+          v-for="country in filteredCountries"
+          :key="country.code"
         >
-          Add to List
-        </button>
+          <img
+            class="h-64 w-auto"
+            v-bind:src="`https://source.unsplash.com/1600x900/?${country.name}-nature`"
+          />
+          <div
+            class="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-gray-700 opacity-0 group-hover:h-full group-hover:opacity-90 duration-500"
+          >
+            <h1 class="text-2xl text-white">
+              {{ country.name }}
+            </h1>
+            <button
+              class="mt-5 px-8 py-3 rounded-full bg-amber-400 hover:bg-amber-600 duration-300"
+              href="#"
+              v-on:click="addCountryToList(country)"
+            >
+              <div class="flex gap-2">
+                <PlusCircleIcon />
+                Add country to My List
+              </div>
+            </button>
+          </div>
+        </div>
+      </main>
+    </div>
+    <div
+      class="flex min-h-screen ml-5 pl-10 w-[400px] bg-gray-800 border-l-4 border-gray-400"
+    >
+      <div class="font-bold py-14 px-6">
+        <div class="flex gap-4">
+          <ListStatusIcon />
+          My List ( {{ store.getters.listCount }} )
+        </div>
+
+        <ul
+          v-for="item in store.state.myList"
+          v-bind:key="item.code"
+          class="mt-10"
+        >
+          <li class="flex gap-1 items-center">
+            {{ item.name }}
+            <button v-on:click="removeCountryFromList(item)">
+              <CloseThickIcon />
+            </button>
+          </li>
+        </ul>
       </div>
     </div>
-  </main>
+  </div>
 </template>
--- src/store/store.js
+++ src/store/store.js
@@ -4,21 +4,29 @@ export default createStore({
   // Define the state of the store.
   state: {
     myList: [],
-    listCount: 0,
+  },
+  // getters are used to compute dereived state based on store state, like a computed property.
+  getters: {
+    listCount: (state) => state.myList.length,
   },
   //mutations are used to update the state of the store, with syncronous code.
   mutations: {
     addCountryToList(state, payload) {
       if (!state.myList.includes(payload)) {
-        state.listCount++;
         state.myList = [...state.myList, payload];
       }
+    },
+    removeCountryFromList(state, payload) {
+      state.myList = state.myList.filter((country) => country !== payload);
     },
   },
   //actions are used to call mutations with asyncronous code.
   actions: {
     actionAddCountryToList(context, payload) {
       context.commit("addCountryToList", payload);
+    },
+    actionRemoveCountryFromList(context, payload) {
+      context.commit("removeCountryFromList", payload);
     },
   },
 });
